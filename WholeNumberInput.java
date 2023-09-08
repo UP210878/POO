6fd9ed17ce1d06c1d43936307be7c95cb70995bb
V1nc3nt <112640971+UP210878@users.noreply.github.com>
2023-09-08 10:53:14 -0600

    Create WholeNumberInput.java

---
+++ WholeNumberInput.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class WholeNumberInput
+{
+	public static void main(String[] args) 
+	{
+	    int[] vec = {20,45,76,1,34,567,423,6,3,5};
+	    Scanner input = new Scanner(System.in);
+		int index;
+		boolean continueCycle = true;
+		do{
+		try{
+		    System.out.print("Input a value between 0 and 9 ");
+		    index = input.nextInt();
+		    System.out.print ("In the index " + index + " the value stored is " + vec[index]);
+		    continueCycle = false;
+		}
+		catch(InputMismatchException ex){
+		    System.out.println("The value must be an integer");
+		    input.nextLine();
+		}
+		catch(IndexOutOfBoundsException ex){
+		    System.out.println("The value must be between 0 and 9");
+		}
+		}while (continueCycle);
+	}
+}
